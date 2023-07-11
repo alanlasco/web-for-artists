c4ac0560013a064ae3a56ee2ef5a8eb27b0c5415
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-07-11 20:40:14 -0300

    gallery added made with fancybox, Jquery

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ $(".nav-item").mouseenter(function(){
 $(".link").mouseenter(function(){
     $(".link").css("color","black");
 })
+
+$(".contenedor-boton-vermas").click(function(){
+    for(let i=4; i <= 7; i++){
+        $(".gallery").append(`<div id="no${i}" class="grid-item"><a href="images/lilpeep${i}.jpg" data-fancybox="gallery"><img src="images/lilpeep${i}.jpg"/></a></div>`);
+    }
+    $(".contenedor-boton-vermas").hide();
+    $(".contenedor-boton-vermenos").css("display","flex")
+})
+$(".contenedor-boton-vermenos").click(function(){
+    for(let i=7; i>=4; i--){
+        $(`#no${i}`).hide();
+    }
+    $(".contenedor-boton-vermenos").css("display", "none");
+    $(".contenedor-boton-volveraver").css("display","flex");
+});
+$(".contenedor-boton-volveraver").click(function(){
+    for(let i=4; i<=7;i++){
+        $(`#no${i}`).show();
+    }
+    $(".contenedor-boton-vermenos").css("display", "flex");
+    $(".contenedor-boton-volveraver").css("display", "none");
+
+})
