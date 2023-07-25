472e797c87138534c59e6e7bfbdcf340cd8c0a2c
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-07-25 01:03:06 -0300

    probando github pages

---
+++ docs/main.js
@@ -0,0 +1,36 @@
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+carousel.interval = 350;
+$(".carousel-item").mouseenter(function(){
+    carousel.pause;
+})
+
+$(".nav-item").mouseenter(function(){
+    $(".nav-link").css("color","black");
+})
+$(".link").mouseenter(function(){
+    $(".link").css("color","black");
+})
+
+$(".contenedor-boton-vermas").click(function(){
+    for(let i=4; i <= 7; i++){
+        $(".gallery").append(`<div id="no${i}" class="grid-item"><a href="images/lilpeep${i}.jpg" data-fancybox="gallery"><img src="images/lilpeep${i}.jpg"/></a></div>`);
+    }
+    $(".contenedor-boton-vermas").hide();
+    $(".contenedor-boton-vermenos").css("display","flex")
+})
+$(".contenedor-boton-vermenos").click(function(){
+    for(let i=7; i>=4; i--){
+        $(`#no${i}`).hide();
+    }
+    $(".contenedor-boton-vermenos").css("display", "none");
+    $(".contenedor-boton-volveraver").css("display","flex");
+});
+$(".contenedor-boton-volveraver").click(function(){
+    for(let i=4; i<=7;i++){
+        $(`#no${i}`).show();
+    }
+    $(".contenedor-boton-vermenos").css("display", "flex");
+    $(".contenedor-boton-volveraver").css("display", "none");
+
+})
